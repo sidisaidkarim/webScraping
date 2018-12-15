29d4efb6c0abb03ff62906518e1737deb3bff97e
sidisaidkarim <sskarim85@gmail.com>
2018-12-15 15:31:18 +0100

    add wp plugin names to csv file

--- blogscraping.py
+++ blogscraping.py
@@ -2,9 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 from csv import writer
 
-resqponse = requests.get('https://medium.com/')
-
-soup = BeautifulSoup(resqponse.text,'html.parser')
-articles = soup.select('.ui-h3')
-for article in articles:
-    print(article.get_text())
+url = 'https://wordpress.org/plugins/search/ecommerce/page/{}/'
+for i in range(2,20):
+    page = i
+    url_page = url.format(page)
+    response = requests.get(url_page)
+    soup = BeautifulSoup(response.text,'html.parser')
+   
+    with open('ecommerce_plugins_wp.csv','w') as csv_file:
+        csv_writer = writer(csv_file)
+        headers = ['plugin name']
+        csv_writer.writerow(headers)
+        plugins = soup.select('.entry')
+        for plugin in plugins:
+            name = plugin.find('a').get_text().replace('-',' ')
+            csv_writer.writerow([name])
+    
+    
