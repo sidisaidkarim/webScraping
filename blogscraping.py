99ee2f92416004d79a138ae09082d3349898668e
sidisaidkarim <sskarim85@gmail.com>
2018-12-15 19:16:13 +0100

    find plugins name by catagory

--- blogscraping.py
+++ blogscraping.py
@@ -2,20 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 from csv import writer
 
-url = 'https://wordpress.org/plugins/search/ecommerce/page/{}/'
-for i in range(2,20):
+url = 'https://wordpress.org/plugins/ecommerce/marketing/page/{}/'
+plug_list = []
+for i in range(2,110):
     page = i
     url_page = url.format(page)
     response = requests.get(url_page)
     soup = BeautifulSoup(response.text,'html.parser')
-   
-    with open('ecommerce_plugins_wp.csv','w') as csv_file:
-        csv_writer = writer(csv_file)
-        headers = ['plugin name']
-        csv_writer.writerow(headers)
-        plugins = soup.select('.entry')
-        for plugin in plugins:
-            name = plugin.find('a').get_text().replace('-',' ')
-            csv_writer.writerow([name])
+    plugins = soup.select('.entry')
+    for plugin in plugins:
+        name = plugin.find('a').get_text().replace('\n',' ')
+        plug_list.append(name)
+
+
+with open('wp_ecommerce_plugins.csv','w',encoding="utf-8") as csv_file:
+    csv_writer = writer(csv_file)
+    headers = ['plugin name']
+    csv_writer.writerow(headers)
+    for pg in plug_list:
+        csv_writer.writerow([pg])
+        
+  
     
     
